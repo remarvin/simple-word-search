6e45d5389f40d34fa50bdb6637ab254d64f130d0
Ripley Marvin <ripleyem@hotmail.com>
2018-07-05 20:37:26 -0700

    Set up initial development environment

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// ES6 content goes here
+
+var greeting = "Hello, world";
+
+function sayHello() {
+  console.log(greeting);
+}
+
+sayHello();
---
+++ src/main.js
@@ -0,0 +1,9 @@
+// ES6 content goes here
+
+let greeting = "Hello, world";
+
+function sayHello() {
+  console.log(greeting);
+}
+
+sayHello();
